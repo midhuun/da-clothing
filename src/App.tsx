fdafa99975b6c264ec58938d09c886e3420c8972
Midhun Kumar <midhun2031@gmail.com>
2025-06-06 08:30:41 +0530

    Add  Maintenance

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Item from "./components/Item";
-import Home from "./Home";
-import SubCategories from "./subCategories/subCategories";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Item from './components/Item';
+import Home from './Home';
+import SubCategories from './subCategories/subCategories';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SubItems from './subCategories/subItems';
 function App() {
+  if (true) {
+    return (
+      <div className="body">
+        <div className="container">
+          <h1>DA Clothing</h1>
+          <p className="maintenance-text">Our website is currently undergoing maintenance.</p>
+          <p>We'll be back shortly. Thank you for your patience!</p>
+        </div>
+      </div>
+    );
+  }
   return (
     <BrowserRouter>
-      <div className=''>
+      <div className="">
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/item/:id" element={<Item />} />
           <Route path="/contact" element={<Header />} />
           <Route path="/categories/:category" element={<SubCategories />} />
-          <Route path="/categories/:category/:subcategory" element={<SubItems/>} />
-          <Route path="/categories/:category/:subcategory/:item" element={<Item/>} />
+          <Route path="/categories/:category/:subcategory" element={<SubItems />} />
+          <Route path="/categories/:category/:subcategory/:item" element={<Item />} />
         </Routes>
         <Footer />
       </div>
